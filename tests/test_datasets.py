6a6824a4ab4238a9c572c1e9c723665ce51c8e25
Domen Mohorcic <d.mohorcic@gmail.com>
2023-08-08 13:50:06 -0400

    fixed datasets tests

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -5,18 +5,20 @@ from pdgrapher import Dataset
 
 class TestDataset(unittest.TestCase):
 
+    dataset = None
+
     def test_creation(self):
-        self.dataset = Dataset(
+        self.__class__.dataset = Dataset(
             forward_path="data/rep-learning-approach-3/processed/real_lognorm/data_forward_A549.pt",
             backward_path="data/rep-learning-approach-3/processed/real_lognorm/data_backward_A549.pt",
             splits_path="data/splits/genetic/A549/random/1fold/splits.pt"
         )
 
     def test_indices(self):
-        self.dataset._test_indices()
+        self.__class__.dataset._test_indices()
     
     def test_dataloaders(self):
-        dataloaders = self.dataset.get_dataloaders()
+        dataloaders = self.__class__.dataset.get_dataloaders()
         self.assertEqual(len(dataloaders), 6)
 
 
