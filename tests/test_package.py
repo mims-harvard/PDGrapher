0a110ae8634340d4ff4ad9eb7605f0cf51923f83
Domen Mohorcic <d.mohorcic@gmail.com>
2023-08-09 13:29:45 -0400

    test package

---
+++ tests/test_package.py
@@ -0,0 +1,25 @@
+import unittest
+
+from torch import load as torch_load
+
+from pdgrapher import PDGrapher, Dataset, Trainer
+
+
+class TestPackage(unittest.TestCase):
+
+    def test_package(self):
+        dataset = Dataset(
+            forward_path="data/rep-learning-approach-3/processed/real_lognorm/data_forward_A549.pt",
+            backward_path="data/rep-learning-approach-3/processed/real_lognorm/data_backward_A549.pt",
+            splits_path="data/splits/genetic/A549/random/1fold/splits.pt"
+        )
+        edge_index = torch_load("data/rep-learning-approach-3/processed/real_lognorm/edge_index_A549.pt")
+        model = PDGrapher(edge_index)
+        trainer = Trainer()
+
+        model_performance = trainer.train(model, dataset, 5)
+
+
+if __name__ == "__main__":
+    unittest.TestLoader.sortTestMethodsUsing = None
+    unittest.main()
