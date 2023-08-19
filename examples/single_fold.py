57615b70438a7cd881eb54f954b48c0205f02724
Domen Mohorcic <d.mohorcic@gmail.com>
2023-08-19 19:23:18 -0400

    another experiment

--- examples/single_fold.py
+++ examples/single_fold.py
@@ -14,7 +14,7 @@ def main():
         fabric_kwargs={"accelerator": "cuda"},
         log=True, use_forward_data=True, use_backward_data=True, use_supervision=True,
         use_intervention_data=True, supervision_multiplier=0.01,
-        log_train=False, log_test=True
+        log_train=False, log_test=True, logging_name="test_"
     )
 
     train_metrics = trainer.train(model, dataset, 50)
