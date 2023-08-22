828261bc12613f3c5fac0da07e4351f54380d4c9
Domen Mohorcic <d.mohorcic@gmail.com>
2023-08-22 16:08:08 -0400

    single fold testing

--- example.bash
+++ example.bash
@@ -11,4 +11,4 @@ module load python/3.10.11
 module load gcc/9.2.0 cuda/11.7
 
 source "venv/bin/activate"
-/n/cluster/bin/job_gpu_monitor.sh & python3 examples/single_fold.py
+/n/cluster/bin/job_gpu_monitor.sh & python3 examples/single_fold.py -f 0
