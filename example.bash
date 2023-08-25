46793191b3182a157b63a034f8c9b453ddf242dd
Domen Mohorcic <d.mohorcic@gmail.com>
2023-08-25 13:04:00 -0400

    sbatch script

--- example.bash
+++ example.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH -c 1
-#SBATCH -t 1-00:00
+#SBATCH -c 2
+#SBATCH -t 5-00:00
 #SBATCH -p gpu
 #SBATCH --mem=32G
 #SBATCH --gres=gpu:1
@@ -11,4 +11,4 @@ module load python/3.10.11
 module load gcc/9.2.0 cuda/11.7
 
 source "venv/bin/activate"
-/n/cluster/bin/job_gpu_monitor.sh & python3 examples/test_folds.py
+/n/cluster/bin/job_gpu_monitor.sh & python3 examples/multiple_folds.py
