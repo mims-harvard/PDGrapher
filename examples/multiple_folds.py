347defa917233871549917e9f9ea2d136cc20072
Domen Mohorcic <d.mohorcic@gmail.com>
2023-08-25 13:42:47 -0400

    fixed example

--- examples/multiple_folds.py
+++ examples/multiple_folds.py
@@ -15,13 +15,13 @@ def main():
     )
 
     edge_index = torch.load("data/rep-learning-approach-3/processed/real_lognorm/edge_index_A549.pt")
-    model = PDGrapher(edge_index, model_args={"n_layers_nn": 1, "n_layers_gnn": 1})
+    model = PDGrapher(edge_index, model_kwargs={"n_layers_nn": 1, "n_layers_gnn": 1, "num_vars": dataset.get_num_vars()})
 
     trainer = Trainer(
         fabric_kwargs={"accelerator": "cuda"},
         log=True, use_forward_data=True, use_backward_data=True, use_supervision=True,
         use_intervention_data=True, supervision_multiplier=0.01,
-        log_train=False, log_test=False
+        log_train=False, log_test=True
     )
 
     model_performances = trainer.train_kfold(model, dataset, 50)
