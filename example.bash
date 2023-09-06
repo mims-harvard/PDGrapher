24610e1935d3587d448eeeb1eff12e980c60559c
Domen Mohorcic <d.mohorcic@gmail.com>
2023-09-06 19:24:46 -0400

    fixed bash

--- example.bash
+++ example.bash
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH -c 2
+#SBATCH -c 1
 #SBATCH -t 1-00:00
 #SBATCH -p gpu
 #SBATCH --mem=32G
 #SBATCH --gres=gpu:1
-#SBATCH -o examples/%j.out
-#SBATCH -e examples/%j.err
+#SBATCH -o examples/tuned_%j.out
+#SBATCH -e examples/tuned_%j.err
 
 module load python/3.10.11
 module load gcc/9.2.0 cuda/11.7
 
 source "venv/bin/activate"
-/n/cluster/bin/job_gpu_monitor.sh & python3 examples/test_folds.py
+/n/cluster/bin/job_gpu_monitor.sh & python3 examples/single_fold.py
